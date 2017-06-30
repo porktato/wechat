cfaf452585270587b69275036f59c727b1a29a58
porktato <13581580171@163.com>
2017-06-30 18:34:51 +0800

    handlle

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print "hello world"
+# -*- coding: utf-8 -*-
+# filename: main.py
+import web
+
+urls = (
+    '/weixin', 'Handle',
+)
+
+class Handle(object):
+    def GET(self):
+        return "hello, this is a test"
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
